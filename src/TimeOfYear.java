fa3d0d9ab36b3a3e0dfa1cb28fb5b07c021f71c6
RyzhkouskiTim <tim.ryzhkouski@gmail.com>
2022-12-28 17:18:02 +0300

    Final version

--- src/TimeOfYear.java
+++ src/TimeOfYear.java
@@ -5,19 +5,11 @@ public class TimeOfYear {
         System.out.println("Введите номер месяца: ");
         Scanner sc=new Scanner(System.in);
         int q= sc.nextInt();
-        switch (q){
-            case 1,2,12:
-                System.out.println("Зима");
-                break;
-            case 3,4,5:
-                System.out.println("Весна");
-                break;
-            case 6,7,8:
-                System.out.println("Лето");
-                break;
-            case 9,10,11:
-                System.out.println("Осень");
-                break;
+        switch (q) {
+            case 1, 2, 12 -> System.out.println("Зима");
+            case 3, 4, 5 -> System.out.println("Весна");
+            case 6, 7, 8 -> System.out.println("Лето");
+            case 9, 10, 11 -> System.out.println("Осень");
         }
 
     }
