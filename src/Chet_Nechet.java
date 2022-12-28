241f74d1e5884910e0829355963f410d62058aec
RyzhkouskiTim <tim.ryzhkouski@gmail.com>
2022-12-28 17:50:26 +0300

    Made_2

---
+++ src/Chet_Nechet.java
@@ -0,0 +1,3 @@
+public class Chet_Nechet {
+
+}
---
+++ src/Tempreture.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Tempreture {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        if (t > -5) {
+            System.out.println("Тепло");
+        } else if (t < -5 && t > -20) {
+            System.out.println("Нормально");
+        } else if (t < -20) {
+            System.out.println("Холодно");
+        }
+    }
+}
